6e3766e11839c95dbc2b10f196cb7c77f6429ef0
Saida_Zhakieva <s.zhakieva24@apec.edu.kz>
2026-05-14 13:26:19 +0500

    Add files via upload

---
+++ schoolapp-master/schoolapp/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace schoolapp;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCheckClicked(object sender, EventArgs e)
+    {
+        count++;
+        StatusLabel.Text = $"Checks: {count}";
+    }
+}
